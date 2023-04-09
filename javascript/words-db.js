c64810212ddd9bbe32fbdae5fb2aa3b1fac1afc2
Ignacio Lapenta <106827571+Lapenta07@users.noreply.github.com>
2023-04-09 04:32:11 -0300

    Add files via upload

---
+++ javascript/words-db.js
@@ -0,0 +1,338 @@
+let wordList = [
+    {
+        word: "banana",
+        hint: "yellow fruit"
+    },
+    {
+        word: "candy",
+        hint: "sweet treat"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "gpu",
+        hint: "computer component"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "venice",
+        hint: "famous city of waters"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "dance",
+        hint: "move to music"
+    },
+    {
+        word: "eagle",
+        hint: "bird of prey"
+    },
+    {
+        word: "grape",
+        hint: "small fruit"
+    },
+    {
+        word: "honey",
+        hint: "sweet syrup"
+    },
+    {
+        word: "jelly",
+        hint: "fruit spread"
+    },
+    {
+        word: "lemon",
+        hint: "sour citrus"
+    },
+    {
+        word: "melon",
+        hint: "sweet fruit"
+    },
+    {
+        word: "olive",
+        hint: "small fruit"
+    },
+    {
+        word: "peach",
+        hint: "soft fruit"
+    },
+    {
+        word: "queen",
+        hint: "female ruler"
+    },
+    {
+        word: "rabbit",
+        hint: "furry animal"
+    },
+    {
+        word: "spider",
+        hint: "eight-legged bug"
+    },
+    {
+        word: "tiger",
+        hint: "big cat"
+    },
+    {
+        word: "uncle",
+        hint: "father's brother"
+    },
+    {
+        word: "virus",
+        hint: "sickness"
+    },
+    {
+        word: "water",
+        hint: "clear liquid"
+    },
+    {
+        word: "xenon",
+        hint: "chemical element"
+    },
+    {
+        word: "yacht",
+        hint: "boat"
+    },
+    {
+        word: "bread",
+        hint: "baked food"
+    },
+    {
+        word: "cheese",
+        hint: "dairy product"
+    },
+    {
+        word: "donkey",
+        hint: "horse relative"
+    },
+    {
+        word: "eggnog",
+        hint: "holiday drink"
+    },
+    {
+        word: "fridge",
+        hint: "appliance"
+    },
+    {
+        word: "guitar",
+        hint: "stringed instrument"
+    },
+    {
+        word: "hockey",
+        hint: "ice sport"
+    },
+    {
+        word: "igloo",
+        hint: "snow house"
+    },
+    {
+        word: "jacket",
+        hint: "clothing item"
+    },
+    {
+        word: "kettle",
+        hint: "water boiler"
+    },
+    {
+        word: "hojas",
+        hint: "parte de un arbol"
+    },
+    {
+        word: "cucaracha",
+        hint: "insecto"
+    },
+    {
+        word: "spaghetti",
+        hint: "pasta"
+    },
+    {
+        word: "naranja",
+        hint: "citrico"
+    },
+    {
+        word: "pimienta",
+        hint: "especia"
+    },
+    {
+        word: "mysql",
+        hint: "sistema de database"
+    },
+    {
+        word: "iran",
+        hint: "pais"
+    },
+    {
+        word: "flauta",
+        hint: "instrumento musical"
+    },
+    {
+        word: "crypto",
+        hint: "criptomonedas"
+    },
+    {
+        word: "tesla",
+        hint: "propiedad de elon musk"
+    },
+    {
+        word: "marte",
+        hint: "planeta del sistema solar"
+    },
+    {
+        word: "proxy",
+        hint: "relacionado a aplicacon de servidores"
+    },
+    {
+        word: "email",
+        hint: "relacionado a intercambio de correos"
+    },
+    {
+        word: "html",
+        hint: "lenguaje de etiquetas web"
+    },
+    {
+        word: "aire",
+        hint: "relacionado a lo gaeoso"
+    },
+    {
+        word: "idea",
+        hint: "pensamiento o sugerencia"
+    },
+    {
+        word: "servidor",
+        hint: "relacionado a sistemas"
+    },
+    {
+        word: "svg",
+        hint: "formato de imagenes en vectores"
+    },
+    {
+        word: "jpeg",
+        hint: "formato de imagen"
+    },
+    {
+        word: "buscar",
+        hint: "acto de encontrar algo"
+    },
+    {
+        word: "llave",
+        hint: "pequeño elemento de metal"
+    },
+    {
+        word: "egypt",
+        hint: "pais"
+    },
+    {
+        word: "joker",
+        hint: "enemigo de batman"
+    },
+    {
+        word: "dubai",
+        hint: "pais"
+    },
+    {
+        word: "foto",
+        hint: "copia de una situación"
+    },
+    {
+        word: "nilo",
+        hint: "rio mas largo del mundo"
+    },
+    {
+        word: "lluvia",
+        hint: "relacionado al agua"
+    },
+    {
+        word: "python",
+        hint: "lenguaje de programación"
+    },
+    {
+        word: "guitarra",
+        hint: "instrumento musical"
+    },
+    {
+        word: "objetivo",
+        hint: "proposito o intencion"
+    },
+    {
+        word: "venus",
+        hint: "planeta del sistema solar"
+    },
+    {
+        word: "gold",
+        hint: "metal precioso de color amarillo"
+    },
+    {
+        word: "ebay",
+        hint: "sitio de compras on line"
+    },
+    {
+        word: "javascript",
+        hint: "lenguaje de programación"
+    },
+    {
+        word: "programar",
+        hint: "relacionado a programacion"
+    },
+    {
+        word: "matrix",
+        hint: "película de cs ficción"
+    },
+    {
+        word: "bugs",
+        hint: "errores relacionados a programacion"
+    },
+    {
+        word: "avatar",
+        hint: "pelicula epica de cs ficción"
+    },
+    {
+        word: "gif",
+        hint: "formate de imagen"
+    },
+    {
+        word: "mental",
+        hint: "relacionado a la mente"
+    },
+    {
+        word: "mapa",
+        hint: "diagrama que representa un area"
+    },
+    {
+        word: "isla",
+        hint: "tierra rodeada de agua"
+    },
+    {
+        word: "hockey",
+        hint: "juego con palos y bochas"
+    },
+    {
+        word: "chess",
+        hint: "deporte/juego de mesa?"
+    },
+    {
+        word: "Facebook",
+        hint: "red social"
+    },
+]
